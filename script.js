bfe77eb65a18187104f53f7a06dda822b8ec9804
Anthony Osiol <funnynoun@gmail.com>
2020-03-15 16:14:48 -0400

    added code for the date and time

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
-var writeNine = document.getElementById("#")
+
+
+// var writeNine = document.getElementById("#")
 
 
 
 
-
+let is24HourTime = false
 // set and display current month in header
-moment().format("MMM Do YY");
 
-// choose line id by hour to write in notes, press button to save note
-
-
-
+let m = moment();
+let formatDate = m.format("MMMM Do YYYY h:mma");
+$("#hodie").text(formatDate);
 
 
+// loop through the table rows to assign the correct time to the 1st column
+$("#calendar tr").each(function (ind, tr) {
+
+    let timeVal = $(tr).attr("data-time");
+    let tableHeader = $(tr).find("th");
+
+    setTimeColumn(timeVal, tableHeader, is24HourTime)
+    setBackground(m, tr, timeVal)
+
+});
+
+//  assigning the correct time to the table column with optional 12hr and 24hr display
+function setTimeColumn(currentHour, tableHeader, is24H) {
+    tableHeader.text(currentHour + ":00");
+    // todo handle optional formating for 12 hour clock, 
+}
+
+function setBackground(currentMoment, tableRow, rowHour) {
+    let currentHour = get24HourFromMoment(currentMoment);
+    let rowHourNum = parseInt(rowHour, 10);
+
+    if (rowHourNum < currentHour) {
+        $(tableRow).addClass("past-time");
+    }
+
+    else if (rowHourNum === currentHour) {
+        $(tableRow).addClass("current-time");
+    }
+
+    else {
+
+        $(tableRow).addClass("future-time");
+    }
+}
 
 
 
-// hours passed id grey,  current hour id blue, future id green 
+
+function get24HourFromMoment(mo) {
+    let hourText = mo.format("H");
+    return parseInt(hourText, 10);
+}
+
+
 
 
 
