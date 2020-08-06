e67ca2057f5c86abd9bb5f0255ef93a70f1ed547
Anthony Osiol <funnynoun@gmail.com>
2020-08-06 19:49:52 -0400

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-Interactive Calendar
+# Interactive Calendar
 
 This is a calendar that represents the correct day and has a table of usual business hours. The user has a place to write in a "to do" at each hour and has option to save. As the day progresses, the hours that have passed turn dark blue, current hour is light grey and hours in the future are color coded darker grey.
 
+![Interactive Calendar](/images/interactive-calendar.png)
 
+### Technologies Used
 
+- JavaScript
+- jQuery
+- BootStrap
+
+### Link
+
+- https://aosiol.github.io/Interactive-Calendar/
--- script.js
+++ script.js
@@ -1,62 +1,62 @@
+$(document).ready(function () {
+   
+    // save button
+    var saveBtn = $(".button");
+   
+    saveBtn.on("click", function (tr) {
+        ;
+    })
 
 
-// var writeNine = document.getElementById("#")
 
 
 
 
-let is24HourTime = false
-// set and display current month in header
+    let is24HourTime = false
+    // set and display current month in header
 
-let m = moment();
-let formatDate = m.format("MMMM Do YYYY h:mma");
-$("#hodie").text(formatDate);
+    let m = moment();
+    let formatDate = m.format("MMMM Do YYYY h:mma");
+    $("#hodie").text(formatDate);
 
 
-// loop through the table rows to assign the correct time to the 1st column
-$("#calendar tr").each(function (ind, tr) {
+    // loop through the table rows to assign the correct time to the 1st column
+    $("#calendar tr").each(function (ind, tr) {
 
-    let timeVal = $(tr).attr("data-time");
-    let tableHeader = $(tr).find("th");
+        let timeVal = $(tr).attr("data-time");
+        let tableHeader = $(tr).find("th");
 
-    setTimeColumn(timeVal, tableHeader, is24HourTime)
-    setBackground(m, tr, timeVal)
+        setTimeColumn(timeVal, tableHeader, is24HourTime)
+        setBackground(m, tr, timeVal)
+
+    });
+
+    //  assigning the correct time to the table column with optional 12hr and 24hr display
+    function setTimeColumn(currentHour, tableHeader, is24H) {
+        tableHeader.text(currentHour + ":00");
+        // todo handle optional formating for 12 hour clock, 
+    }
+
+    function setBackground(currentMoment, tableRow, rowHour) {
+        let currentHour = get24HourFromMoment(currentMoment);
+        let rowHourNum = parseInt(rowHour, 10);
+
+        if (rowHourNum < currentHour) {
+            $(tableRow).addClass("past-time");
+        } else if (rowHourNum === currentHour) {
+            $(tableRow).addClass("current-time");
+        } else {
+            $(tableRow).addClass("future-time");
+        }
+    }
+
+
+    function get24HourFromMoment(mo) {
+        let hourText = mo.format("H");
+        return parseInt(hourText, 10);
+    }
 
 });
-
-//  assigning the correct time to the table column with optional 12hr and 24hr display
-function setTimeColumn(currentHour, tableHeader, is24H) {
-    tableHeader.text(currentHour + ":00");
-    // todo handle optional formating for 12 hour clock, 
-}
-
-function setBackground(currentMoment, tableRow, rowHour) {
-    let currentHour = get24HourFromMoment(currentMoment);
-    let rowHourNum = parseInt(rowHour, 10);
-
-    if (rowHourNum < currentHour) {
-        $(tableRow).addClass("past-time");
-    }
-
-    else if (rowHourNum === currentHour) {
-        $(tableRow).addClass("current-time");
-    }
-
-    else {
-
-        $(tableRow).addClass("future-time");
-    }
-}
-
-
-
-
-function get24HourFromMoment(mo) {
-    let hourText = mo.format("H");
-    return parseInt(hourText, 10);
-}
-
-
 
 
 
